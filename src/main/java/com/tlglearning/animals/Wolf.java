990041ce80d709841f13d548a276a4b4697a7ae2
Wint-1234 <winton.ebanks@yahoo.com>
2022-09-20 13:07:44 -0400

    Initialization demonstration.

--- src/main/java/com/tlglearning/animals/Wolf.java
+++ src/main/java/com/tlglearning/animals/Wolf.java
@@ -2,15 +2,41 @@ package com.tlglearning.animals;
 
 public class Wolf {
 
+  private static final int MY_CONSTANT = initializeValue();
+
+  private int data = initializeData();
+
+  static {
+    System.out.println("Wolf static initialization block");
+  }
+
+  {
+    System.out.println("Wolf instance initialization block");
+  }
+
+  public Wolf() {
+    System.out.println("Wolf constructor");
+  }
+
+  private static int initializeValue() {
+    System.out.println("Wolf.initializationValue");
+    return -1;
+  }
+
+  public static void whoAmI() {
+    System.out.println("I am a wolf");
+  }
+
+  private int initializeData() {
+    System.out.println("Wolf.initializeData");
+    return 1;
+  }
+
   public void vocalize() {
     System.out.println("Howl like a wolf!");
   }
 
   public void hunt() {
     System.out.println("Hunt in packs for live prey");
-  }
-
-  public static void whoAmI() {
-    System.out.println("I am a wolf");
   }
 }
